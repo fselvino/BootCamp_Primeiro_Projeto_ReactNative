421d2c524e043f6cc312cc78502037c662f35089
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-02-29 23:17:46 -0300

    React Navigation

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,39 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
+import React from 'react';
 import './config/ReactotronConfig';
+import Routes from './routes';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.body}>
-        <Text style={styles.nome}>Fernando Selvino de </Text>
-      </View>
-    );
-  }
+export default function App() {
+  return <Routes />;
 }
-console.tron.log('Vamos debugar');
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#7159c1',
-  },
-  nome: {
-    fontSize: 26,
-    color: 'black',
-    borderBottomLeftRadius: 50,
-  },
-});
